271b4de4734e4bd9fe302d14f00ae99a4a19cadd
NyAary <manananyaary@gmail.com>
2025-09-16 16:33:55 +0300

    First portofolio 1 setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://nyaary29.github.io',
+  base:'MyPortofolio',
+  trailingSlash:'always',
   integrations: [tailwind()],
 });
